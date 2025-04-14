96400a0628a34f3c5732fe1c2b451d077a71f9ad
Dima Kolesov <kolesov.public@gmail.com>
2025-04-15 00:34:38 +0300

    initial

---
+++ LazyStateMachine/LazyStateMachineApp.swift
@@ -0,0 +1,17 @@
+//
+//  LazyStateMachineApp.swift
+//  LazyStateMachine
+//
+//  Created by Dima Kolesov on 15.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct LazyStateMachineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
